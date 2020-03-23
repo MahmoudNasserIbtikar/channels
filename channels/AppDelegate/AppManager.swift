b7e23ead3c910ff06243c513ce9bae46489c3802
Mahmoud Nasser <mahmoud.eisa@ibtikar.net.sa>
2020-03-23 18:59:32 +0200

    ## Task : Create Application manager
    - create AppManager class that handles the appDelegate window and the SceneDelegate windowScene
    
    - Remove Pre created viewController class
    - Remove main storyboard
    - Create Home view controller class and nib
    - update the info.plist file not to work with main.storyboard
    - make The home view controller the window root

---
+++ channels/AppDelegate/AppManager.swift
@@ -0,0 +1,40 @@
+//
+//  AppManager.swift
+//  channels
+//
+//  Created by Mahmoud Nasser on 3/23/20.
+//  Copyright © 2020 Mahmoud Nasser. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppManager: NSObject {
+    static let shared = AppManager()
+    
+    var window: UIWindow?
+    
+    static func launchApp(_ application: UIApplication) {
+        if #available(iOS 13, *) {
+        } else {
+            initWindow()
+        }
+    }
+    
+    static func initWindow() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.shared.window = window
+    }
+    
+    @available(iOS 13.0, *)
+    static func initSceneWindow(_ windowScene: UIWindowScene) {
+        let window = UIWindow(windowScene: windowScene)
+        let vc = HomeViewController()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.shared.window = window
+    }
+}
